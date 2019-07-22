f972b9108ea455854507a24ebd13a283b951ebf5
vinay sharma <vinaysharma1259@gmail.com>
2019-07-22 18:16:22 +0530

    Initial Commit

---
+++ Assessment_Xebia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assessment_Xebia
+//
+//  Created by Vinay Sharma on 22/07/19.
+//  Copyright © 2019 Vinay Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
